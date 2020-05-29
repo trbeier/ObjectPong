92eeba882dbd42e150215576ba4e73c358e31ae9
Tony Beier <tony@beiersoftware.com>
2020-05-29 12:01:59 -0700

    First Upload

---
+++ ObjectPong/js/Animation.js
@@ -0,0 +1,169 @@
+﻿// Basic Code for Animations
+// By:  Tony Beier May 10 - 20, 2020
+
+
+// ***** Start Key Codes Constants *****
+const leftArrowKey = 37;
+const upArrowKey = 38;
+const rightArrowKey = 39;
+const downArrowKey = 40;
+// ***** End Key Codes Constants *****
+
+
+// ***** Start Setup the Canvas *****
+class Canvas {
+
+}
+var canvas = document.getElementById('canvas1');
+var Canvas2D = canvas.getContext('2d');
+
+function drawDot(col, row) { Canvas2D.fillRect(col, row, 1, 1); }
+function drawLine(col1, row1, col2, row2) {
+    Canvas2D.beginPath();
+    Canvas2D.moveTo(col1, row1);
+    Canvas2D.lineTo(col2, row2);
+    Canvas2D.stroke();
+}
+function drawCircle(col, row, radius) {
+    Canvas2D.beginPath();
+    Canvas2D.arc(col, row, radius, 2 * Math.PI, false);
+}
+function drawBall(col, row, radius) {
+    drawCircle(col, row, radius);
+    Canvas2D.fill();
+}
+
+function LeftOfCanvas(col) { return (col < 0); }
+function CenterColOfCanvas(itemWidth=0) { return (canvas.width - itemWidth) / 2; }
+function RightOfCanvas(col) { return (col > canvas.width); }
+
+function AboveCanvas(row) { return (row < 0); }
+function CenterRowOfCanvas(itemHeight = 0) { return (canvas.height - itemHeight) / 2; }
+function BelowCanvas(row) { return (row > canvas.height); }
+// ***** End Setup the Canvas *****
+
+
+// ***** Start Basic Animation Setup *****
+var animate = window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	function (callback) { window.setTimeout(callback, 1000 / 60) };
+
+// These routines will be overridden;
+var UpdateEverything = function () { };
+var DrawEverything = function () { };
+
+// The animation cycle:  Update, Draw, Wait for next refresh and then do it all again!
+var RefreshTheScreen = function () {
+	UpdateEverything();
+	DrawEverything();
+	animate( RefreshTheScreen);
+};
+
+// Start the animation;
+animate(RefreshTheScreen);
+
+// ***** End Basic Animation Setup *****
+
+
+// ***** Start Character Object *****
+const NoCollission = null;
+var characterList = [];
+
+class Character {
+    constructor(col, row, width, height, color, shape) {
+        this.col = col;
+        this.row = row;
+
+        this.width = width;
+        this.height = height;
+
+        this.color = color;
+
+        this.isRectangle = (shape.toLowerCase().startsWith("r"));
+        this.isBall = (shape.toLowerCase().startsWith("b"));
+        if (!this.isBall && !this.isRectangle)
+            alert("Unknown shape " + shape);
+
+        this.colSpeed = 0;
+        this.rowSpeed = 0;
+
+        this.characterNumber = characterList.push(this) -1;
+    }
+
+    get colCenter() { return this.col + (this.width / 2); }
+    get rowCenter() { return this.row + (this.height / 2); }
+    get top() { return this.row - (this.isBall ? this.radius : 0); }
+    get bottom() { return this.row + (this.isBall ? this.radius : this.height); }
+    get left() { return this.col - (this.isBall ? this.radius : 0); }
+    get right() { return this.col + (this.isBall ? this.radius : this.width); }
+
+    DistanceToMyRow(thatRow) {
+        var ret;
+        if (this.row == thatRow)
+            ret = 0;  // We are already there
+        else if (this.row > thatRow)
+            ret = this.top - thatRow;
+        else
+            ret = thatRow - this.bottom;
+        return ret;
+    }
+    HeadingToMyRow(that) {
+        if (this.row == that.row)
+            return false;  // We are already there
+        else if (this.row > that.row)
+            return that.rowSpeed > 0;
+        else
+            return that.rowSpeed < 0;
+        // This might work????
+        // return Math.sign(this.row - that.row) == Math.sign(that.rowSpeed);
+    }
+
+    Collision(that) {
+        // Don't check if it collides with itself
+        if (this.WeAreTheSame(that))
+            return false;
+        else
+            return (this.top < that.bottom && this.bottom > that.top
+                && this.left < that.right && this.right > that.left);
+    }
+    WeHaveCollidedWith() {
+        for (var indeX = 0; indeX < characterList.length; indeX++) {
+            if (this.Collision(characterList[indeX]))
+                return characterList[indeX];
+        }
+        // If we get here there was no collission.
+        return NoCollission;
+    }
+
+    DrawIt() {
+        Canvas2D.fillStyle = this.color;
+        if (this.isRectangle)
+            Canvas2D.fillRect(this.col, this.row, this.width, this.height);
+        else if (this.isBall) {
+            Canvas2D.beginPath();
+            Canvas2D.arc(this.col, this.row, this.radius, 2 * Math.PI, false);
+            Canvas2D.fill();
+        }
+        else
+            alert("Unknown shape");
+    }
+
+    MoveIt() {
+        this.col += this.colSpeed;
+        this.row += this.rowSpeed;
+    }
+
+    WeAreTheSame(that) {
+        return this.characterNumber == that.characterNumber;
+    }
+}
+// ***** End Character Object *****
+
+
+// get random whole numbers in a specific range
+// @see https://stackoverflow.com/a/1527820/2124254
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+// get random whole numbers in a specific range
---
+++ ObjectPong/js/Pong.js
@@ -0,0 +1,278 @@
+﻿// Pong Clone In JavaScript
+// By: Matt Mongeau
+// https://jsfiddle.net/kHJr6/2/
+// Modified by:  Tony Beier May 10, 2020
+
+const maxPaddleSpeed = 4;
+const paddleHeight = 10;
+const paddleWidth = 50;
+
+const topPlayer = -1;
+var topPlayerScore = document.getElementById("topPlayerScore");
+
+const bottomPlayer = 1;
+var bottomPlayerScore = document.getElementById("bottomPlayerScore");
+
+const ballSize = 5;
+
+class Paddle extends Character {
+    constructor(playerPosition, playerImageID) {
+        var row = 0;
+
+        if (playerPosition === bottomPlayer)
+            row = canvas.height - (paddleHeight * 2);
+        else if (playerPosition === topPlayer)
+            row = paddleHeight;
+        else
+            alert("Invalid Player #" + playerPosition);
+
+        super(CenterColOfCanvas(paddleWidth), row, paddleWidth, paddleHeight, "#0000FF", "rectangle");
+
+        this.playerPosition = playerPosition;
+
+        if (playerImageID == undefined || playerImageID == null || playerImageID == "")
+            this.playerImage = null;
+        else {
+            this.playerImage = document.getElementById(playerImageID);
+            if (playerPosition == topPlayer)
+                this.playerImage.className = "topPlayer";
+            else
+                this.playerImage.className = "bottomPlayer";
+        }
+
+        this.colSpeed = 0;
+        this.rowSpeed = 0;
+
+        this.wins = 0;
+    }
+
+    CheckSpeed() {
+        // Reduce speed to the allowed maximum (no cheating)
+        if (this.colSpeed < 0 && this.colSpeed < -maxPaddleSpeed) { // max speed left
+            this.colSpeed = -maxPaddleSpeed;
+        } else if (this.colSpeed > 0 && this.colSpeed > maxPaddleSpeed) { // max speed right
+            this.colSpeed = maxPaddleSpeed;
+        }
+    }
+
+    DrawIt() {
+        super.DrawIt()
+
+        if (this.playerImage != null)
+            this.playerImage.style.left = this.col + "px";
+    }
+
+    MoveIt() {
+        this.CheckSpeed();
+
+        super.MoveIt();
+
+        if (LeftOfCanvas(this.col)) {
+            this.col = 0;
+            this.colSpeed = 0;
+        } else if (RightOfCanvas(this.right)) {
+            this.col = canvas.width - this.width;
+            this.colSpeed = 0;
+        }
+    }
+
+    Wins() {
+        this.wins++;
+        if (this.playerPosition == topPlayer)
+            topPlayerScore.innerHTML = "Top: " + this.wins;
+        else
+            bottomPlayerScore.innerHTML = "Bottom: " + this.wins;
+    }
+}
+
+class Player extends Paddle {
+    constructor(playerPosition, playerImageID) {
+        super(playerPosition);
+    }
+
+    Update(ball) {
+        for (var key in keysDown) {
+            var value = Number(key);
+            if (value == leftArrowKey) {
+                this.colSpeed = -maxPaddleSpeed;
+            } else if (value == rightArrowKey) {
+                this.colSpeed = maxPaddleSpeed;
+            } else {
+                this.colSpeed = 0;  // stop moving
+            }
+            this.MoveIt();
+        }
+    }
+}
+
+
+
+// *** Computer AI ****
+class Computer extends Paddle {
+    constructor(playerPosition) {
+        super(playerPosition);
+    }
+
+    Update(ball) {
+        this.colSpeed = ball.col - this.colCenter;
+
+        this.MoveIt();
+    }
+}
+
+// *** Tony AI ****
+class Tony extends Paddle {
+    constructor(playerPosition) {
+        super(playerPosition);
+        this.playerImage = document.getElementById("tony");
+    }
+
+    Update(ball) {
+        if (this.HeadingToMyRow(ball)) {
+            this.colSpeed = ball.col - this.colCenter;
+            this.colSpeed *= 2;
+            this.CheckSpeed();
+
+            var rowDistance = Math.abs(this.row - ball.row) / canvas.height;
+            if (rowDistance < 0.25)
+                this.colSpeed /= 1;
+            else
+                if (rowDistance < 0.5)
+                    this.colSpeed /= 2;
+                else
+                    if (rowDistance < 0.75)
+                        this.colSpeed /= 3;
+                    else
+                        this.colSpeed /= 4;
+        } else {
+            this.colSpeed = CenterColOfCanvas(this.width) - this.col;
+            this.colSpeed = Math.sign(this.colSpeed);
+        }
+
+        this.MoveIt();
+    }
+}
+
+
+// *** Cheating  AI ****
+class Cheater extends Paddle {
+    constructor(playerPosition) {
+        super(playerPosition);
+    }
+
+    Update(ball) {
+        if (this.HeadingToMyRow(ball)) {
+            if (!this.weHaveCalculated) {
+                this.weHaveCalculated = false;
+
+                var simCol = ball.col;
+                var simColSpeed = ball.colSpeed;
+                var simRow = ball.row;
+
+                // Simulate the balls movement.
+                while (this.DistanceToMyRow(simRow) > 0) {
+                    simCol += simColSpeed;
+                    simRow += ball.rowSpeed;
+
+                    if (LeftOfCanvas(simCol + ball.radius)) {
+                        simCol = ball.radius;
+                        simColSpeed = -simColSpeed;
+                    } else if (RightOfCanvas(simCol - ball.radius)) {
+                        simCol = canvas.width - ball.radius;
+                        simColSpeed = -simColSpeed;
+                    }
+                }
+                // Calculate where we have to be.
+                this.colSpeed = simCol - this.colCenter;
+                // Calculate how much time we have to get there.
+                this.colSpeed /= this.DistanceToMyRow(ball.row) / Math.abs(ball.rowSpeed);
+            }
+        } else {
+            this.weHaveCalculated = false;
+            this.colSpeed = CenterColOfCanvas(this.width) - this.col;
+            // Calculate how much time we have to get there.
+            this.colSpeed = Math.sign(this.colSpeed);
+        }
+
+        this.MoveIt();
+    }
+}
+
+
+class Ball extends Character {
+    constructor(col, row) {
+        super(col, row, ballSize * 2, ballSize * 2, "#000000", "ball");
+
+        this.colSpeed = 1;
+        this.rowSpeed = 3;
+        this.radius = ballSize;
+    }
+
+    Reset() {
+        this.colSpeed = 1;
+        this.rowSpeed = 3;
+        this.col = canvas.width / 2;
+        this.row = canvas.height / 2;
+    }
+
+    Update(paddle1, paddle2) {
+        super.MoveIt();
+
+        if (AboveCanvas(this.row)) {
+            paddle1.Wins();
+            this.Reset();
+            return;
+        } else if (BelowCanvas(this.row)) {
+            paddle2.Wins();
+            this.Reset();
+            return;
+        }
+
+        if (LeftOfCanvas(this.left)) {
+            this.col = this.radius;
+            this.colSpeed = -this.colSpeed;
+        } else if (RightOfCanvas(this.right)) {
+            this.col = canvas.width - this.radius;
+            this.colSpeed = -this.colSpeed;
+        }
+
+        var collideWith = this.WeHaveCollidedWith();
+        if (collideWith != NoCollission) {
+            this.colSpeed += Math.sign(this.colSpeed) * Math.random() * (maxPaddleSpeed / 2);
+            this.rowSpeed = -this.rowSpeed;
+            if (Math.abs(this.rowSpeed) < this.height / 2)
+                this.rowSpeed += Math.sign(this.rowSpeed);
+            this.row += this.rowSpeed;
+        }
+    }
+}
+
+var keysDown = {};
+
+var player1 = new Cheater(bottomPlayer);
+var player2 = new Cheater(topPlayer);
+var ball = new Ball(canvas.width / 2, canvas.height / 2);
+
+var DrawEverything = function () {
+    Canvas2D.fillStyle = "#FF00FF";
+    Canvas2D.fillRect(0, 0, canvas.width, canvas.height);
+    player1.DrawIt();
+    player2.DrawIt();
+    ball.DrawIt();
+}
+
+
+var UpdateEverything = function () {
+    player1.Update(ball);
+    player2.Update(ball);
+    ball.Update(player1, player2);
+}
+
+
+window.addEventListener("keydown", function (event) {
+    keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function (event) {
+    delete keysDown[event.keyCode];
+});
